3a035ae8ad62582b345991bb430ac8324bff29dd
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-16 02:10:17 +0100

    delete extra files, add input colors, change font style

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ var fs = require('fs');
 const path = require("path");
 var pixelWidth = require('string-pixel-width');
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-
 app.use(express.static(path.join(__dirname, "public")));
 
-// define a route handler for the default home page
-app.get("/:text1-:text2-:color", (req, res) => {
-  console.log(req.params);
-  const { text1, text2, color } = req.params;
-  textSize1 = pixelWidth(text1, { size: 16 }) + 20;
-  textSize2 = pixelWidth(text2, { size: 16 }) + 20;
-  const width = pixelWidth(text1, { size: 16 }) + 20;
-  // res.render("index", {...req.params, textSize1: width, textSize2: pixelWidth(text2, {size: 16}) + 20});
-  res.header('Content-Type', 'image/svg+xml');
-  // '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="400" height="180"> <rect x="50" y="20" rx="20" ry="20" width="150" height="150" style="fill:red;stroke: black;stroke-width:5;opacity:0.5" /> </svg> ')
+function convertColor(color) {
+  return "#" + color;
+}
 
+function getPixelWidth(text, isLetterSpacing) {
+  if(isLetterSpacing) {
+    return (pixelWidth(text, { size: 13, bold: true }) + 22) * 1.2;
+  }
+  return pixelWidth(text, { size: 13, bold: true }) + 20;
+
+}
+
+// define a route handler for the default home page
+app.get("/:text1-:text2-:color1-:color2-:bgColor1-:bgColor2", (req, res) => {
+  console.log(req.params);
+  const { text1, text2, color1, color2, bgColor1, bgColor2 } = req.params;
+  const [textColor1, textColor2, boxColor1, boxColor2] =[convertColor(color1), convertColor(color2), convertColor(bgColor1), convertColor(bgColor2)];
+  const textSize1 = getPixelWidth(text1, false);
+  const textSize2 = getPixelWidth(text2, true);
+  
+  res.header('Content-Type', 'image/svg+xml');
   
   res.send(Buffer.from(`<svg 
   xmlns='http://www.w3.org/2000/svg' 
@@ -30,16 +37,18 @@ app.get("/:text1-:text2-:color", (req, res) => {
   height='28'
   style='border-radius: 10px; overflow:hidden'
   >
-  <style>
-    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300");
-  </style>
   <g shape-rendering="crispEdges">
-    <rect width='${textSize1}' height="28" fill='${color}'></rect>
-    <rect x='${textSize1}' width='${textSize2}' height="28" fill="red"></rect>
+    <rect width='${textSize1}' height="28" fill='${boxColor1}'></rect>
+    <rect x='${textSize1}' width='${textSize2}' fill='${boxColor2}' height="28"></rect>
     </g>
-    <g>
-    <text font-family="-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji" x='11' y='20'>${text1}</text>
-    <text font-family="-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji" x='${textSize1+10}' y='20'>${text2}</text>
+    <g font-size="11">
+    <text 
+    font-weight="bold"
+    fill='${textColor1}' font-family="-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji" x='10' y='18'>${text1.toUpperCase()}</text>
+    <text
+      font-weight="bold"
+      letter-spacing="1.5"
+    fill='${textColor2}' font-family="-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji" x='${textSize1+10}' y='18'>${text2.toUpperCase()}</text>
     </g>
 </svg>`));
 });
